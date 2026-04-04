bac3b49d726c3b0ef2dad0823592e0ba4d404aad
Reaper-ai <gauravupreti232+github@gmail.com>
2026-04-04 16:44:05 +0530

    cleanup

--- mobile/hooks/use-color-scheme.ts
+++
@@ -1,6 +0,0 @@
-import { useColorScheme as useRNColorScheme } from 'react-native';
-
-export function useColorScheme() {
-  const systemScheme = useRNColorScheme();
-  return systemScheme ?? 'light';
-}
